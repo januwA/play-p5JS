05783c43c1d99b054cabb46a7ac56063866e3dd8
januwA <ajanuw1641845087@sina.com>
2017-12-05 20:50:48 +0800

    or 循环

--- 2017-12-4/sketch.js
+++ 2017-12-4/sketch.js
@@ -1,23 +1,35 @@
-var x = 0;
-var speed = 3;
+var objLeft;
+var objRight;
 
 function setup() {
 	createCanvas(600, 400);
+	objLeft = new Obj({x:0,speed:3});
+	objRight = new Obj({x:width,speed:-3});
 }
 
 function draw() {
 	background(0)
-	stroke('white')
-	strokeWeight(2)
-	noFill()
+	objLeft.show()
+	objRight.show()
+}
 
-	ellipse(x,200,100)
-	if(x > width || x < 0){
-		speed = speed * -1
-		// -3 * -1
-		// 3
-		// 3 * -1
-		// -3
+class Obj {
+	constructor(props) {
+		this.x = props.x;
+		this.speed = props.speed;
 	}
-	x += speed;
+	show(){
+		stroke('white')
+		strokeWeight(2)
+		noFill()
+		ellipse(this.x,200,100)
+		if(this.x > width || this.x < 0){
+			this.speed = this.speed * -1
+			// -3 * -1
+			// 3
+			// 3 * -1
+			// -3
+		}
+		this.x += this.speed;
+	}
 }
