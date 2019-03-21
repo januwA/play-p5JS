4fb0c72aa0954a25d0dd0bc13a09cfb59d95fc94
januwA <ajanuw1641845087@sina.com>
2019-03-21 14:34:17 +0800

    生成固定数量的粒子对象

--- 2017-12-23/sketch.js
+++ 2017-12-23/sketch.js
@@ -1,40 +1,38 @@
-var balls = []
+var balls = [];
 
 function setup() {
-	createCanvas(600, 400);
+  createCanvas(600, 400);
 
-	for(let i =0; i< 200; i++){
-		balls.push( new Ball() )
-	}
-
+  for (let i = 0; i < 200; i++) {
+    balls.push(new Ball());
+  }
 }
 
 function draw() {
-	background(0);
+  background(0);
 
-	for(let i=0; i<balls.length; i++){
-		balls[i].run();
-	}
-
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].run();
+  }
 }
 
 class Ball {
-	constructor(){
-		this.x = random(0, width)
-		this.y = random(0, height)
-	}
-	run(){
-		this.display()
-		this.move()
-	}
-	display(){
-		stroke(random(0, 255), random(0, 255), random(0, 255))
-		noFill()
-		ellipse(this.x, this.y ,24,24)
-	}
+  constructor() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+  }
+  run() {
+    this.display();
+    this.move();
+  }
+  display() {
+    stroke(random(0, 255), random(0, 255), random(0, 255));
+    noFill();
+    ellipse(this.x, this.y, 24, 24);
+  }
 
-	move(){
-		this.x += random(-1,1)
-		this.y += random(-1,1)
-	}
+  move() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+  }
 }
