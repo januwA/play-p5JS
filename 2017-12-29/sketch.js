3fc9ccf43951917e8e1a2e08aa2bb05f6de1a435
januwA <ajanuw1641845087@sina.com>
2019-03-21 14:37:59 +0800

    多个对象的碰撞检测

--- 2017-12-29/sketch.js
+++ 2017-12-29/sketch.js
@@ -1,28 +1,27 @@
-var balls = []
+var balls = [];
 var len = 10;
 
 function setup() {
   createCanvas(600, 400);
   for (let i = 0; i < len; i++) {
-    balls.push(new Ball(random(width), random(height)))
+    balls.push(new Ball(random(width), random(height)));
   }
 }
 
 function draw() {
   background(0);
-	// 两个 for来 处理ball之间的碰撞
+  // 两个 for来 处理ball之间的碰撞
   for (let i = 0; i < balls.length; i++) {
-		balls[i].update();
-		balls[i].show();
-		for (let j = 0; j < balls.length; j++) {
-			// dist 像是在求 弦
-			if (i !=j && balls[i].isIntersects(balls[j])) {
-				balls[i].cahngeColor()
-				balls[j].cahngeColor()
-			}
-		}
-	}
-
+    balls[i].update();
+    balls[i].show();
+    for (let j = 0; j < balls.length; j++) {
+      // dist 像是在求 弦
+      if (i != j && balls[i].isIntersects(balls[j])) {
+        balls[i].cahngeColor();
+        balls[j].cahngeColor();
+      }
+    }
+  }
 }
 
 class Ball {
@@ -30,17 +29,17 @@ class Ball {
     this.x = x;
     this.y = y;
     this.r = 48;
-    this.color = color(255, 0, 255, 150)
+    this.color = color(255, 0, 255, 150);
   }
 
   show() {
-    noStroke()
+    noStroke();
     fill(this.color);
     ellipse(this.x, this.y, this.r, this.r);
   }
   update() {
-    this.x += random(-1, 1)
-    this.y += random(-1, 1)
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
   }
 
   cahngeColor() {
