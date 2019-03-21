5d4e144314f00a399763c9fac2d990a7a747e8d1
januwA <ajanuw1641845087@sina.com>
2019-03-21 14:32:18 +0800

    绘制文字

--- 2017-12-22/sketch.js
+++ 2017-12-22/sketch.js
@@ -1,22 +1,21 @@
-var words = ['aoo', 'boo', 'coo', 'doo']
+var words = ["aoo", "boo", "coo", "doo"];
 var index = 0;
 
-
 function setup() {
-	createCanvas(400, 400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-	background(0)
+  background(0);
 
-	fill('red');
-	textSize(22)
-	text(words[index], 12, 200)
+  fill("red");
+  textSize(22);
+  text(words[index], 12, 200);
 }
 
-function mousePressed(){
-	index += 1
-	if( index === words.length){
-		index = 0
-	}
+function mousePressed() {
+  index += 1;
+  if (index === words.length) {
+    index = 0;
+  }
 }
