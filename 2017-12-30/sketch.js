f3c7c6bdef16f10bcff52c95095045ef11a8b375
januwA <ajanuw1641845087@sina.com>
2019-03-21 14:39:50 +0800

    preload() 预加载大型资源.

--- 2017-12-30/sketch.js
+++ 2017-12-30/sketch.js
@@ -1,33 +1,33 @@
 var img;
 var myimage;
 
-function preload(){
-	//需要在 这个钩子 函数里面加载图片
-	img  = loadImage('https://i.loli.net/2018/09/05/5b8fce8bbe8b5.jpg')
-
+function preload() {
+  //需要在 这个钩子 函数里面加载图片
+  img = loadImage("https://i.loli.net/2018/09/05/5b8fce8bbe8b5.jpg");
 }
+
 function setup() {
-	createCanvas(600, 400);
-	myimage = new myImage(img);
+  createCanvas(600, 400);
+  myimage = new myImage(img);
 }
 
 function draw() {
-	background(0)
-	myimage.update()
-	myimage.show()
+  background(0);
+  myimage.update();
+  myimage.show();
 }
 
-class myImage{
-	constructor(img){
-		this.img = img
-		this.x = 0;
-		this.y = 0;
-	}
-	show(){
-		image(this.img, this.x, this.y, width, height)
-	}
-	update(){
-		this.x += random(-1, 1)
-		this.y += random(-1, 1)
-	}
+class myImage {
+  constructor(img) {
+    this.img = img;
+    this.x = 0;
+    this.y = 0;
+  }
+  show() {
+    image(this.img, this.x, this.y, width, height);
+  }
+  update() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
+  }
 }
