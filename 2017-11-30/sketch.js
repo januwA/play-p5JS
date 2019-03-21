792b30a4a2b3b439bd1193239ca3fdc30f7f06f2
januwA <ajanuw1641845087@sina.com>
2019-03-21 14:26:54 +0800

    在画布上的随机位置生成粒子

--- 2017-11-30/sketch.js
+++ 2017-11-30/sketch.js
@@ -1,43 +1,43 @@
-var pros = []
+var pros = [];
 
 function setup() {
-	createCanvas(600, 300);
+  createCanvas(600, 300);
 }
 
 function draw() {
-	background(0)
-	var pro = new Pro();
-	pros.push( pro);
+  background(0);
+  var pro = new Pro();
+  pros.push(pro);
 
-	for (let i = pros.length-1; i > 0; i--) {
-		pros[i].update();
-		pros[i].show();
-		if(pros[i].remove()){
-			pros.splice(i,1);
-		}
-	}
+  for (let i = pros.length - 1; i > 0; i--) {
+    pros[i].update();
+    pros[i].show();
+    if (pros[i].remove()) {
+      pros.splice(i, 1);
+    }
+  }
 }
 
 class Pro {
-	constructor() {
-		this.x = random(0, width);
-		this.y = random(0, height);
-		this.r = random(50, 255)
-		this.g = random(50, 255)
-		this.b = random(50, 255)
+  constructor() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.r = random(50, 255);
+    this.g = random(50, 255);
+    this.b = random(50, 255);
 
-		this.alpha = random(120,200);
-	}
+    this.alpha = random(120, 200);
+  }
 
-	remove(){
-		return this.alpha < 0
-	}
-	update(){
-		this.alpha -= 0.6
-	}
-	show(){
-		noStroke();
-		fill(this.r, this.g, this.b, this.alpha)
-	  ellipse(this.x,this.y,10)
-	}
+  remove() {
+    return this.alpha < 0;
+  }
+  update() {
+    this.alpha -= 0.6;
+  }
+  show() {
+    noStroke();
+    fill(this.r, this.g, this.b, this.alpha);
+    ellipse(this.x, this.y, 10);
+  }
 }
